b4f18f4439f74e417c836ab0d143d33850c86ca2
Wendelcorp <bryce@brycewendelaar.com>
2016-09-22 16:42:24 -0400

    Added user reward functionality

--- crowdfunder/app/controllers/rewards_users_controller.rb
+++ crowdfunder/app/controllers/rewards_users_controller.rb
@@ -1,8 +1,8 @@
 class RewardsUsersController < ApplicationController
   def create
   @reward = Reward.where(project_id: params[:project_id]).first
-  @user = current_user[:id]
+  @user = current_user
   @rewards_user = RewardsUser.create(user: @user, reward: @reward)
-   end
+  end
 
 end
--- crowdfunder/app/models/rewards_user.rb
+++ crowdfunder/app/models/rewards_user.rb
@@ -1,3 +1,4 @@
 class RewardsUser < ApplicationRecord
-
+  belongs_to :user
+  belongs_to :reward
 end
