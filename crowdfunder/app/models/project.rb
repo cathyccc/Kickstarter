43d9d2c428aea3dc2a8cfa410788a9a08f66a675
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-09-22 18:47:13 -0400

    created method count total pledges

--- crowdfunder/app/models/project.rb
+++ crowdfunder/app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ApplicationRecord
   has_many :rewards
   has_many :users, through: :rewards
   accepts_nested_attributes_for :rewards
+
+  def all_pledges_total
+    rewards.inject(0) {|sum,reward| sum + reward.pledges_total}
+  end
 end
